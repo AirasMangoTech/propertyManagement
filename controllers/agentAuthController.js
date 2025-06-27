77cf995971876509f9ec07912a3eed6f152ca266
saraibaqeel07 <saraibaqeel07@gmail.com>
2025-06-27 18:47:38 +0500

    changes

--- controllers/agentAuthController.js
+++ controllers/agentAuthController.js
@@ -7,13 +7,13 @@ const { sendSuccess, sendError } = require('../helpers/responseHelper');
 const JWT_SECRET = process.env.JWT_SECRET || 'your_secret_key';
 
 exports.signup = async (req, res) => {
-    const { name, email, password,phone,image,address } = req.body;
+    const { name, email, password, phone, image, address } = req.body;
     try {
         const existingUser = await User.findOne({ email });
         if (existingUser) return sendError(res, 'Agent already exists', 400);
 
         const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({ name, email, password: hashedPassword,image,address,phone });
+        const user = new User({ name, email, password: hashedPassword, image, address, phone });
         await user.save();
 
         const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '7d' });
@@ -69,6 +69,7 @@ exports.getAgents = async (req, res) => {
             $or: [
                 { name: { $regex: search, $options: 'i' } },
                 { location: { $regex: search, $options: 'i' } },
+                { email: { $regex: search, $options: 'i' } },
                 { address: { $regex: search, $options: 'i' } },
                 { type: { $regex: search, $options: 'i' } },
                 { purpose: { $regex: search, $options: 'i' } },
